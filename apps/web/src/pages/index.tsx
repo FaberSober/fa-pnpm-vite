eeebca59325d0b04837933128d2f76462eaff847
xu.pengfei <1508075252@qq.com>
2022-12-17 17:25:27 +0800

    add react

---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/**
+ * @author xu.pengfei
+ * @date 2022/12/17 17:23
+ */
+export default function index() {
+  return (
+    <div>Hello</div>
+  )
+}
---
+++ apps/web/vite.config.ts
@@ -0,0 +1,70 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import Pages from 'vite-plugin-pages'
+// import VitePluginImp from "vite-plugin-imp";
+import path from 'path';
+
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  console.log('loadEnv(mode, process.cwd())', loadEnv(mode, process.cwd()))
+  return {
+    plugins: [
+      react(),
+      Pages({
+        exclude: ['**/components/*.tsx', '**/modal/*.tsx'],
+      }),
+      // 使用按需导入后，antd动态样式会失效
+      // 按需导入
+      // https://github.com/onebay/vite-plugin-imp
+      // VitePluginImp({
+      //   libList: [
+      //     {
+      //       libName: 'lodash',
+      //       libDirectory: '',
+      //       camel2DashComponentName: false
+      //     },
+      //     {
+      //       libName: "antd",
+      //       // 不写 libList,默认是按需导入css,
+      //       // 想要使用 less, 需要改为加载 es 的 index.js,
+      //       // 里面加载了组件所需的一些 less 文件
+      //       style: (name) => `antd/es/${name}/style/index.js`,
+      //     },
+      //   ],
+      // }),
+    ],
+    //* css模块化
+    css: {
+      modules: { // css模块化 文件以.module.[css|less|scss]结尾
+        // 回调必须返回 `local`，`global`，或者 `pure`
+        mode: (resourcePath) => {
+          // 形如xx.module.less的样式，会使用local模块化编译。其他的则返回全局样式
+          if (/\.module\.(css|less)$/i.test(resourcePath)) {
+            return "local";
+          }
+          return "global";
+        },
+        generateScopedName: '[name]__[local]___[hash:base64:5]',
+        hashPrefix: 'prefix',
+      },
+    },
+    resolve: {
+      alias: [
+        { find: /^~@/, replacement: path.resolve(__dirname, 'src') },
+        // { find: /^~/, replacement: '' },
+        { find: '@', replacement: path.resolve(__dirname, 'src') },
+        // fix less import by: @import ~
+      ],
+    },
+    // server: {
+    //   open: true,
+    //   proxy: {
+    //     '/api': {
+    //       target: loadEnv(mode, process.cwd()).VITE_APP_BASE_URL,
+    //       changeOrigin: true,
+    //     },
+    //   },
+    // },
+  }
+})
