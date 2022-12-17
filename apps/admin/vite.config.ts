e2b65b9dbc2c4552bd923695381ee381760013b3
xu.pengfei <1508075252@qq.com>
2022-12-17 21:41:14 +0800

    prettier

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import Pages from 'vite-plugin-pages'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import Pages from 'vite-plugin-pages';
 import * as path from 'path';
-
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  console.log('loadEnv(mode, process.cwd())', loadEnv(mode, process.cwd()))
+  console.log('loadEnv(mode, process.cwd())', loadEnv(mode, process.cwd()));
   return {
     plugins: [
       react(),
@@ -16,14 +15,15 @@ export default defineConfig(({ command, mode }) => {
     ],
     //* css模块化
     css: {
-      modules: { // css模块化 文件以.module.[css|less|scss]结尾
+      modules: {
+        // css模块化 文件以.module.[css|less|scss]结尾
         // 回调必须返回 `local`，`global`，或者 `pure`
         mode: (resourcePath) => {
           // 形如xx.module.less的样式，会使用local模块化编译。其他的则返回全局样式
           if (/\.module\.(css|less)$/i.test(resourcePath)) {
-            return "local";
+            return 'local';
           }
-          return "global";
+          return 'global';
         },
         generateScopedName: '[name]__[local]___[hash:base64:5]',
         hashPrefix: 'prefix',
@@ -46,5 +46,5 @@ export default defineConfig(({ command, mode }) => {
     //     },
     //   },
     // },
-  }
-})
+  };
+});
