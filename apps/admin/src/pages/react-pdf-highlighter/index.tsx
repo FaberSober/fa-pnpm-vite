dc9f20f36fb0206769fcaca354c728e11a5191fc
xupengfei <faberxu@gmail.com>
2024-10-06 11:52:58 +0800

    react-pdf-highlighter 可以正常展示pdf

--- apps/admin/src/pages/react-pdf-highlighter/index.tsx
+++ apps/admin/src/pages/react-pdf-highlighter/index.tsx
@@ -1,11 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AreaHighlight, Highlight, IHighlight, PdfHighlighter, PdfLoader, Popup } from 'react-pdf-highlighter';
+import "./index.scss"
+import "react-pdf-highlighter/dist/style.css"
+
 
 /**
  * @author xu.pengfei
  * @date 2024/10/6 10:16
  */
 export default function ReactPdfHighlighter() {
+  const [highlights, setHighlights] = useState<IHighlight[]>([])
+
   return (
-    <div>pdf</div>
+    <div className="fa-main">
+      <div className="fa-panel">
+        1
+      </div>
+      <div className="fa-panel">
+        <div className="fa-pdf-container">
+          <PdfLoader
+            url="/plugins/pdfjs/test.pdf"
+            // workerSrc="/plugins/pdfjs/pdfjs-dist@3.3.122/build/pdf.worker.min.js"
+            beforeLoad={<div>Loading</div>}
+          >
+            {(pdfDocument) => (
+              <PdfHighlighter
+                pdfDocument={pdfDocument}
+                enableAreaSelection={(event) => event.altKey}
+                onScrollChange={() => document.location.hash = ""}
+                scrollRef={(scrollTo) => {
+                  // scrollViewerTo.current = scrollTo;
+                  // scrollToHighlightFromHash();
+                }}
+                onSelectionFinished={(
+                  position,
+                  content,
+                  hideTipAndSelection,
+                  transformSelection,
+                ) => (
+                  <div>onSelectionFinished</div>
+                )}
+                highlightTransform={(
+                  highlight,
+                  index,
+                  setTip,
+                  hideTip,
+                  viewportToScaled,
+                  screenshot,
+                  isScrolledTo,
+                ) => {
+                  const isTextHighlight = !highlight.content?.image;
+
+                  const component = isTextHighlight ? (
+                    <Highlight
+                      isScrolledTo={isScrolledTo}
+                      position={highlight.position}
+                      comment={highlight.comment}
+                    />
+                  ) : (
+                    <AreaHighlight
+                      isScrolledTo={isScrolledTo}
+                      highlight={highlight}
+                      onChange={(boundingRect) => {
+                        console.log(
+                          highlight.id,
+                          {boundingRect: viewportToScaled(boundingRect)},
+                          {image: screenshot(boundingRect)},
+                        );
+                      }}
+                    />
+                  );
+
+                  return (
+                    <Popup
+                      popupContent={<div>{JSON.stringify(highlight)}</div>}
+                      onMouseOver={(popupContent) =>
+                        setTip(highlight, (highlight) => popupContent)
+                      }
+                      onMouseOut={hideTip}
+                      key={index}
+                    >
+                      {component}
+                    </Popup>
+                  );
+                }}
+                highlights={highlights}
+              />
+            )}
+          </PdfLoader>
+        </div>
+      </div>
+    </div>
   )
 }
